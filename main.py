7622da58bb335fc185650f5856469f263df80d05
kaedelily <svlibertini1998@gmail.com>
2025-07-03 05:14:25 -0400

    0.0.5 add calc, and tests

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
 import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
-
 client = genai.Client(api_key=api_key)
 
 if len(sys.argv) < 2:
     print("No input provided. Please provide a prompt as a command line argument.")
     exit(1)
 
-# Check for --verbose flag
-verbose = False
-args = sys.argv[1:]
-if '--verbose' in args:
-    verbose = True
-    args.remove('--verbose')
+user_prompt = sys.argv[1]
 
-if not args:
-    print("No prompt provided. Please provide a prompt as a command line argument.")
-    exit(1)
-
-user_prompt = args[0]
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
@@ -34,9 +23,11 @@ messages = [
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=messages
 )
-if verbose:
-    print(f"User prompt: {user_prompt}")
-print(response.text)
-if verbose and response.usage_metadata:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if "--verbose" in sys.argv:
+    if response.usage_metadata:
+            print(response.text)
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    print(response.text)
