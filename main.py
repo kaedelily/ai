b202637892f49460a27c514cafaf79e6eea4b854
kaedelily <svlibertini1998@gmail.com>
2025-07-04 01:27:22 -0400

    0.1.1 function declarations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 import sys
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,14 +16,41 @@ if len(sys.argv) < 2:
     exit(1)
 
 user_prompt = sys.argv[1]
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
+    model='gemini-2.0-flash-001', contents=system_prompt
 )
+if response.candidates:
+    for candidate in response.candidates:
+        if hasattr(candidate, 'content') and candidate.content and hasattr(candidate.content, 'parts'):
+            for part in candidate.content.parts:
+                if hasattr(part, 'function_call') and part.function_call:
+                    function_call_part = part.function_call
+                    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+                elif hasattr(part, 'text') and part.text:
+                    print(response.text)
+     
 if "--verbose" in sys.argv:
     if response.usage_metadata:
             print(response.text)
